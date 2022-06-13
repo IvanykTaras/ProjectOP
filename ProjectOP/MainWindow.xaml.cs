45082f8454227d685d0bb5da5639958ccb775c22
TarasIvanyk <hanekava4@gmail.com>
2022-06-13 20:19:57 +0200

    Changes

--- ProjectOP/MainWindow.xaml.cs
+++ ProjectOP/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,16 +14,36 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+//Pexels
+using PexelsDotNetSDK.Api;
+using Newtonsoft.Json;
+
 namespace ProjectOP
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+
     public partial class MainWindow : Window
     {
+        private async void DownloadJsonData()
+        {
+
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void Search_Click(object sender, RoutedEventArgs e)
+        {
+            PexelsClient client = new PexelsClient("YOUR_API_KEY");
+            var photos = await client.FeaturedCollectionsAsync(1,3);
+            if (photos != null) { 
+                Log.Text =  Convert.ToString(photos.totalResults);               
+            }
         }
     }
 }
