e8639636759e9174b8816b36bfa1cf020b39c21d
TarasIvanyk <hanekava4@gmail.com>
2022-06-14 01:20:26 +0200

    Fix some bugs with ui header table users

--- ProjectOP/ManageUsers.xaml.cs
+++ ProjectOP/ManageUsers.xaml.cs
@@ -62,8 +62,60 @@ namespace ProjectOP
             var ds = new DataSet();
             da.Fill(ds,"Users");
             DataTable usersTable = ds.Tables["Users"];
+
+
+            var HeaderBlock1 = new Border()
+            {
+                BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#000"),
+                BorderThickness = new Thickness(1),
+                Width = 100,
+                Child = new TextBlock() { Text = "Name", Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#ff0000")  }
+            };
+            var HeaderBlock2 = new Border()
+            {
+                BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#000"),
+                BorderThickness = new Thickness(1),
+                Width = 100,
+                Child = new TextBlock() { Text = "FullName" , Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#ff0000") }
+            };
+            var HeaderBlock3 = new Border()
+            {
+                BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#000"),
+                BorderThickness = new Thickness(1),
+                Width = 100,
+                Child = new TextBlock() { Text = "Password" , Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#ff0000") }
+            };
+            var HeaderBlock4 = new Border()
+            {
+                BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#000"),
+                BorderThickness = new Thickness(1),
+                Width = 100,
+                Child = new TextBlock() { Text = "Phone" , Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#ff0000") }
+            };
+            var stackHeader = new StackPanel() { Orientation = Orientation.Horizontal, };
+            stackHeader.Children.Add(HeaderBlock1);
+            stackHeader.Children.Add(HeaderBlock2);
+            stackHeader.Children.Add(HeaderBlock3);
+            stackHeader.Children.Add(HeaderBlock4);
+            usersStackTable.Children.Add(stackHeader);
+
+
             foreach (DataRow row in usersTable.Rows) {
-                usersStackTable.Children.Add(new TextBlock() { Text = (string)row["uname"]});
+                
+                var block1 = new Border() { BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#000"), BorderThickness = new Thickness(1), Width = 100,
+                    Child = new TextBlock() {Text = (string)row[0] } };
+                var block2 = new Border() { BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#000"), BorderThickness = new Thickness(1), Width = 100,
+                    Child = new TextBlock() {Text = (string)row[1] } };
+                var block3 = new Border() { BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#000"), BorderThickness = new Thickness(1), Width = 100,
+                    Child = new TextBlock() {Text = (string)row[2] } };
+                var block4 = new Border() { BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#000"), BorderThickness = new Thickness(1), Width = 100,
+                    Child = new TextBlock() {Text = (string)row[3] } };
+                var stack = new StackPanel() { Orientation = Orientation.Horizontal};
+                stack.Children.Add(block1);
+                stack.Children.Add(block2);
+                stack.Children.Add(block3);
+                stack.Children.Add(block4);
+                usersStackTable.Children.Add(stack);
             }
             Conn.Close();
             
